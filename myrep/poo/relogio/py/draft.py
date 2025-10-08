5d9fd9943433ce9b0249553283d8d7548251ca33
breGarcia66 <brennol3vy@gmail.com>
2025-10-08 12:18:41 -0300

    voltando pro codespace

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,66 +1,102 @@
 class Watch:
-    def __init__(self, hour: int, minute: int, seconds: int):
-        self.__hour    = self.setHour(hour);
-        self.__minute  = self.setMinute(minute);
-        self.__seconds = self.setSeconds(seconds);
+    def __init__(self, hour: int = 0, minute: int = 0, seconds: int = 0):
+        self.__hour    = 0;
+        self.__minute  = 0;
+        self.__seconds = 0;
+
+        self.setHour(hour);
+        self.setMinute(minute);
+        self.setSeconds(seconds);
 
     def __str__(self) -> str:
-        return f"{self.getHour()}:{self.getMinute()}:{self.getSeconds()}";
-    
+        hour    = self.getHour();
+        minute  = self.getMinute();
+        seconds = self.getSeconds();
+        return f"{hour:02}:{minute:02}:{seconds:02}";
+
     # Métodos de busca
 
-    def getHour(self) -> str:
-        return f"{self.__hour:.2d}";
+    def getHour(self) -> int:
+        return self.__hour;
 
-    def getMinute(self) -> str:
-        return f"{self.__minute:.2d}";
+    def getMinute(self) -> int:
+        return self.__minute;
 
-    def getSeconds(self) -> str:
-        return f"{self.__seconds:.2d}";
+    def getSeconds(self) -> int:
+        return self.__seconds;
 
     # Métodos mutantes
 
-    def setHour(self, value):
+    def setHour(self, value: int):
         if value < 0 or value > 23:
             print("fail: hora invalida");
             return;
 
         self.__hour = value;
     
-    def setMinute(self, value):
+    def setMinute(self, value: int):
         if value < 0 or value > 59:
             print("fail: minuto invalido");
             return;
 
         self.__minute = value;
     
-    def setSeconds(self, value):
+    def setSeconds(self, value:int):
         if value < 0 or value > 59:
             print("fail: segundo invalido");
             return;
 
-        self.__minute = value;
+        self.__seconds = value;
 
     # Método incrementar tempo
 
     def nextScond(self):
-        self.setSeconds(self.setSeconds + 1);
-
-        if self.getSeconds() > 59:
+        if self.getSeconds() != 59:
+            self.setSeconds(self.getSeconds() + 1);
+        
+        else:
             self.setSeconds(0);
-            self.setMinute(self.getMinute() + 1);
-
-        if self.getMinute() > 59:
-            self.setMinute(0);
-            self.setHour(self.getHour() + 1);
-
-        if self.getHour() > 59:
-            self.setHour(0);
-            self.setMinute(0);
-            self.setSeconds(0);
+            if self.getMinute() != 59:
+                self.setMinute(self.getMinute() + 1);
+            
+            else:
+                self.setMinute(0);
+                if self.getHour() != 23:
+                    self.setHour(self.getHour() + 1);
+                else:
+                    self.setHour(0);
+                    self.setMinute(0);
+                    self.setSeconds(0); 
 
         return;
 
-relogio = Watch(16, 59, 30);
+def main():
+    watch = Watch();
 
-print(relogio);
+    while True:
+        line = input();
+        args = line.split(" ");
+        print(f"${line}");
+        
+        match args[0]:
+            case "show":
+                print(watch);
+            
+            case "init":
+                watch = Watch(int(args[1]), int(args[2]), int(args[3]));
+            
+            case "set":
+                watch.setHour(int(args[1]));
+                watch.setMinute(int(args[2]));
+                watch.setSeconds(int(args[3]));
+
+            case "next":
+                watch.nextScond();
+
+            case "end":
+                break;
+            
+            case _:
+                print("fail: comando invalido");
+
+main();
